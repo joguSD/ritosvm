c195e134840131870ee4e4065f1604286f01944d
Jordan Guymon <jogu.sd@gmail.com>
2015-08-15 23:52:20 -0700

    Reworked the downloader to logic to retry on 429s after a sleep

--- src/main/java/com/weasel/svm/MatchDownloader.java
+++ src/main/java/com/weasel/svm/MatchDownloader.java
@@ -2,76 +2,78 @@ package com.weasel.svm;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.Scanner;
 
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class MatchDownloader {
 	private static final String apiKey = "c5bdc00b-25f1-4534-bcaf-edede824ffbf";
 	private static final String urlFormat = "https://%s.api.pvp.net/api/lol/%s/v2.2/match/%d?includeTimeline=false&api_key=%s";
 
-	public static String getMatch(long match, String region){
-		String requestString = String.format(urlFormat, region,region,match,apiKey);
+	public static String getMatch(long match, String region) throws InterruptedException {
+		String requestString = String.format(urlFormat, region, region, match, apiKey);
 		URL url;
-		try {
-			url = new URL(requestString);
-	    	HttpURLConnection hurl = (HttpURLConnection) url.openConnection();
-	    	Scanner scanner = new Scanner(hurl.getInputStream());
-	    	String response = scanner.useDelimiter("\\Z").next();
-	    	scanner.close();
-	    	return response;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
+		while (true) {
+			try {
+				url = new URL(requestString);
+				HttpURLConnection hurl = (HttpURLConnection) url.openConnection();
+				Scanner scanner = new Scanner(hurl.getInputStream());
+				String response = scanner.useDelimiter("\\Z").next();
+				scanner.close();
+				return response;
+			} catch (IOException e) {
+				// if we get a 429 (too many requests) just sleep and retry
+				//FIXME - is there a better way to check if it's a 429...?
+				if (e.getMessage().contains("code: 429")) { 
+					Thread.sleep(12000);
+				} else {
+					e.printStackTrace();
+					return null;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
 		}
 	}
 
 	@SuppressWarnings("unchecked")
-	public static void getMatches(String region){
+	public static void getMatches(String region) {
 		FileWriter file;
 		String response;
-		JSONObject matchJson;
 		JSONParser parser = new JSONParser();
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
 		try {
-			JSONArray json = (JSONArray) parser.parse(new FileReader("./matches/"+region.toUpperCase()+".json"));
-			file = new FileWriter("./matches/"+region.toUpperCase()+"_Objects.json");
+			JSONArray json = (JSONArray) parser.parse(new FileReader("./matches/" + region.toUpperCase() + ".json"));
+			file = new FileWriter("./matches/" + region.toUpperCase() + "_Objects.json");
 
 			int counter = 0;
-			file.write("[");
 			Iterator<Long> it = json.iterator();
-			while(it.hasNext()) {
+
+			// for every match found in the region file
+			while (it.hasNext()) {
+				counter++;
 				long match = it.next();
 				response = getMatch(match, region);
-				if(response != null) {
-					matchJson = (JSONObject) parser.parse(response);
-					file.write(gson.toJson(matchJson));
-					if(it.hasNext()){
-						file.write(",");
-					}
-					counter++;
-					System.out.print(".");
-					if(counter % 20 == 0) {
-						System.out.print("\n");
-					}
+				// if response is not null write the json string to file
+				if (response != null) {
+					file.write(response);
+					if (it.hasNext()) file.write("\n");
+				}		
+				System.out.print(response != null ? "." : "x");
+				if (counter % 50 == 0) {
+					System.out.print("\n");
 				}
-				Thread.sleep(1350);
 			}
-			file.write("]");
 			file.close();
-			System.out.println("Downloaded: "+counter+" matches.");
+			System.out.println("Downloaded: " + counter + " matches.");
 		} catch (Exception e) {
 			e.printStackTrace();
-			return;
 		}
 	}
 }
